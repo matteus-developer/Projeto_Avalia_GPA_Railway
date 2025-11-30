09e95b217d8b00d59c34dd64062b3b0cd42c4052
Mateus Filgueiras <mateusfilgueiras232@gmail.com>
2025-11-30 14:29:29 -0300

    Update Professor.java

--- src/main/java/com/example/model/Professor.java
+++ src/main/java/com/example/model/Professor.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.FetchType;
 
 @Entity
 public class Professor {
