c926f1e8c11ea7d2af0dce5978c709f2fead259a
Mateus Filgueiras <mateusfilgueiras232@gmail.com>
2025-12-02 23:04:15 -0300

    Add existsByMatriProfessor method to ProfessorRepository

--- src/main/java/com/example/repository/ProfessorRepository.java
+++ src/main/java/com/example/repository/ProfessorRepository.java
@@ -4,14 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.model.Professor;
 
 public interface ProfessorRepository extends JpaRepository<Professor, Integer>{
-	
-	//Busca o professor pela matricula
+
 	public Professor findBymatriProfessor(String matriProfessor);
 	
-	//Busca o professor pelo email
+
 	public Professor findByemailProfessor(String emailProfessor);
 	
-	//Busca o professor pelo seu idProfessor
+
 	public Professor findByIdProfessor(int idProfessor);
+
+	public boolean existsByMatriProfessor(String matriProfessor);
 
 }
